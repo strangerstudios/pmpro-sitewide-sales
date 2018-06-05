154f442a4f9bbc0e6d7fbd967ecd2d605263a034
kimcoleman <kim@strangerstudios.com>
2018-06-05 14:37:36 -0400

    Updating plugin naming, links, action links, activation message

--- pmpro-sitewide-sale.php
+++ pmpro-sitewide-sale.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name: Paid Memberships Pro - Sitewide Sale
- * Plugin URI: https://www.paidmembershipspro.com/add-ons/pmpro-slack-integration/
- * Description: Run a sitewide sale (Black Friday, Cyber Monday, etc) with Paid Memberships Pro
+ * Plugin Name: Paid Memberships Pro - Sitewide Sale Add On
+ * Plugin URI: https://www.paidmembershipspro.com/add-ons/sitewide-sale/
+ * Description: Run a sitewide sale (Black Friday, Cyber Monday, etc.) with Paid Memberships Pro
  * Author: strangerstudios, dlparker1005
  * Author URI: https://www.paidmembershipspro.com
  * Version: .1
@@ -22,7 +22,6 @@ require_once PMPROSWS_DIR . '/includes/settings.php';
 require_once PMPROSWS_DIR . '/includes/automatic-discount.php';
 require_once PMPROSWS_DIR . '/includes/banners.php';
 
-
 add_action( 'admin_enqueue_scripts', 'pmpro_sws_admin_scripts' );
 /**
  * Enqueues selectWoo
@@ -34,3 +33,65 @@ function pmpro_sws_admin_scripts() {
 	wp_register_style( 'selectWooCSS', '/wp-content/plugins/pmpro-sitewide-sale/css/selectWoo' . $suffix . '.css' );
 	wp_enqueue_style( 'selectWooCSS' );
 }
+
+/**
+ * Runs only when the plugin is activated.
+ *
+ * @since 0.1.0
+ */
+function pmpro_sws_admin_notice_activation_hook() {
+	// Create transient data.
+	set_transient( 'pmpro-sws-admin-notice', true, 5 );
+}
+register_activation_hook( PMPROSWS_BASENAME, 'pmpro_sws_admin_notice_activation_hook' );
+
+/**
+ * Admin Notice on Activation.
+ *
+ * @since 0.1.0
+ */
+function pmpro_sws_admin_notice() {
+	// Check transient, if available display notice.
+	if ( get_transient( 'pmpro-sws-admin-notice' ) ) { ?>
+		<div class="updated notice is-dismissible">
+			<p><?php printf( __( 'Thank you for activating. <a href="%s">Visit the settings page</a> to get started with the Sitewide Sale Add On.', 'pmpro-sitewide-sale' ), get_admin_url( null, 'admin.php?page=pmpro-sitewide-sale' ) ); ?></p>
+		</div>
+		<?php
+		// Delete transient, only display this notice once.
+		delete_transient( 'pmpro-sws-admin-notice' );
+	}
+}
+add_action( 'admin_notices', 'pmpro_sws_admin_notice' );
+
+/**
+ * Function to add links to the plugin action links
+ *
+ * @param array $links Array of links to be shown in plugin action links.
+ */
+function pmpro_sws_plugin_action_links( $links ) {
+	if ( current_user_can( 'manage_options' ) ) {
+		$new_links = array(
+			'<a href="' . get_admin_url( null, 'admin.php?page=pmpro-sidewide-sale' ) . '">' . __( 'Settings', 'pmpro-sidewide-sale' ) . '</a>',
+		);
+	}
+	return array_merge( $new_links, $links );
+}
+add_filter( 'plugin_action_links_' . PMPROBP_BASENAME, 'pmpro_sws_plugin_action_links' );
+
+/**
+ * Function to add links to the plugin row meta
+ *
+ * @param array  $links Array of links to be shown in plugin meta.
+ * @param string $file Filename of the plugin meta is being shown for.
+ */
+function pmpro_sws_plugin_row_meta( $links, $file ) {
+	if ( strpos( $file, 'pmpro-sitewide-sale.php' ) !== false ) {
+		$new_links = array(
+			'<a href="' . esc_url( 'https://www.paidmembershipspro.com/add-ons/sitewide-sale/' ) . '" title="' . esc_attr( __( 'View Documentation', 'pmpro' ) ) . '">' . __( 'Docs', 'pmpro-sitewide-sale' ) . '</a>',
+			'<a href="' . esc_url( 'https://www.paidmembershipspro.com/support/' ) . '" title="' . esc_attr( __( 'Visit Customer Support Forum', 'pmpro' ) ) . '">' . __( 'Support', 'pmpro-sitewide-sale' ) . '</a>',
+		);
+		$links = array_merge( $links, $new_links );
+	}
+	return $links;
+}
+add_filter( 'plugin_row_meta', 'pmpro_sws_plugin_row_meta', 10, 2 );
--- readme.txt
+++ readme.txt
@@ -2,25 +2,27 @@
 Contributors: strangerstudios, dlparker1005
 Tags: paid memberships pro, pmpro, memberships, ecommerce
 Requires at least:
-Tested up to:
+Tested up to: 4.9.6
 Stable tag: .1
 
-Run a sitewide sale (Black Friday, Cyber Monday, etc) with Paid Memberships Pro.
+Run a sitewide sale (Black Friday, Cyber Monday, etc.) with Paid Memberships Pro.
 
 == Description ==
+
 This plugin requires Paid Memberships Pro to function.
 
-Allows you to choose a discount code and homepage to associate with your sale, automatically applying the discount_code
-only to users who visit the sale homepage. Also has the option to automatically generate banners to put on your site
-to advertise your sale.
+The Sitewide Sale Add On allows you to create flash or sitewide sales. A settings page allows you to create a new sale, assign a discount code and unique landing page for the promotion. The plugin will automatically apply a discount code for users who visit the sale landing page. 
+
+The plugin also adds the option to display sitewide page banners to advertise your sale.
 
 == Installation ==
 
 1. Upload the `pmpro-sitewide-sale` directory to the `/wp-content/plugins/` directory of your site.
 1. Activate the plugin through the 'Plugins' menu in WordPress.
-1. Navigate to the `Sitewide Sale` settings page by going to `Memberships > Sitewide Sale`
-1. Choose a discount code, sale page and banner settings
+1. Navigate to the `Sitewide Sale` settings page by going to `Memberships > Sitewide Sale`.
+1. Choose a discount code, sale page and banner settings.
 
 == Changelog ==
+
 = .1 =
 * Initial Version
