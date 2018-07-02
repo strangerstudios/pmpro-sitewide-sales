db6c24c332d5307ab179b664d32fa97e9cc3edda
Jason Coleman <jason@strangerstudios.com>
2018-07-02 16:04:37 -0400

    dismiss link in bottom right banner. need to NOT set color in final version though. and also maybe persist the dismissal.

--- includes/banners/bottom-right.php
+++ includes/banners/bottom-right.php
@@ -30,8 +30,18 @@ function pmpro_sws_show_bottom_right_banner() {
 			margin: 0 0 10px 0;
 			padding: 0;
 		}
+
+		#pmpro_sws_banner_bottom_right a.dismiss, a.dismiss:active, a.dismiss:link, a.dismiss:visited {
+			color: #FFF;
+			float: right;
+			text-decoration: none;
+		}
+		#pmpro_sws_banner_bottom_right a.dismiss:hover {
+			text-decoration: underline;
+		}
 	</style>
 	<div id="pmpro_sws_banner_bottom_right" class="pmpro_sws_banner">
+		<a href="javascript:void(0);" onclick="document.getElementById('pmpro_sws_banner_bottom_right').style.display = 'none';" class="dismiss">x</a>
 		<h3><?php _e( $options['banner_title'] ); ?></h3>
 		<?php echo apply_filters( 'the_content', $options['banner_description'] ); ?>
 		<a class="pmpro_btn" href="<?php echo get_permalink( $options['landing_page_post_id'] ); ?>"><?php _e( $options['link_text'] ); ?></a>
