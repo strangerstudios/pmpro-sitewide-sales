fc09a89d0812578a905e87eec8712da02d03e747
PBrocks <paul@barthmaier.rocks>
2018-07-12 15:56:31 -0400

    adding back filter but like function name

--- includes/banners/bottom-right.php
+++ includes/banners/bottom-right.php
@@ -10,8 +10,7 @@ function pmpro_sws_show_bottom_right_banner() {
 	<div id="pmpro_sws_banner_bottom_right" class="pmpro_sws_banner">
 		<a href="javascript:void(0);" onclick="document.getElementById('pmpro_sws_banner_bottom_right').style.display = 'none';" class="dismiss">x</a>
 		<h3><?php _e( $options['banner_title'] ); ?></h3>
-		<?php echo apply_filters( 'pmpro_sws_banner_bottom', $options['banner_description'] ); ?>
-		<?php echo wpautop( $options['banner_description'] ); ?>
+		<?php echo apply_filters( 'pmpro_sws_banner_bottom_right', $options['banner_description'] ); ?>
 		<a class="pmpro_btn" href="<?php echo get_permalink( $options['landing_page_post_id'] ); ?>"><?php _e( $options['link_text'] ); ?></a>
 	</div> <!-- end pmpro_sws_banner -->
 	<?php
