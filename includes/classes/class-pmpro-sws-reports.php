157328c3aba06848988430d0f2a58f7256f520f4
David Parker <dparker837@verizon.net>
2018-08-08 09:43:23 -0400

    Moved calculated function names outside of namespaces

--- includes/classes/class-pmpro-sws-reports.php
+++ includes/classes/class-pmpro-sws-reports.php
@@ -172,8 +172,8 @@ class PMPro_SWS_Reports {
 					<tr>
 						<td>' . esc_html__( 'Sitewide Sale', 'pmpro_sitewide_sale' ) . '</td>
 						<td>' . esc_html( get_the_title( $sitewide_sale_id ) ) .
-						' (' . date_i18n( get_option( 'date_format' ), ( new DateTime( get_post_meta( $sitewide_sale_id, 'start_date', true ) ) )->format( 'U' ) ) .
-						' - ' . date_i18n( get_option( 'date_format' ), ( new DateTime( get_post_meta( $sitewide_sale_id, 'end_date', true ) ) )->format( 'U' ) ) . ')</td>
+						' (' . date_i18n( get_option( 'date_format' ), ( new \DateTime( get_post_meta( $sitewide_sale_id, 'start_date', true ) ) )->format( 'U' ) ) .
+						' - ' . date_i18n( get_option( 'date_format' ), ( new \DateTime( get_post_meta( $sitewide_sale_id, 'end_date', true ) ) )->format( 'U' ) ) . ')</td>
 					</tr>
 				</thead>
 				<tbody>';
@@ -285,42 +285,4 @@ class PMPro_SWS_Reports {
 			return -1;
 		}
 	}
-}
-
-/**
- * Report Widget, needs to be outside class because
- * function name is 'calculated'in core
- */
-function pmpro_report_pmpro_sws_reports_widget() {
-	echo PMPro_SWS_Reports::get_report_for_code();
-}
-
-/**
- * Report Page, needs to be outside class because
- * function name is 'calculated'in core
- */
-function pmpro_report_pmpro_sws_reports_page() {
-	global $wpdb;
-	$options = PMPro_SWS_Settings::pmprosws_get_options();
-	$sitewide_sales = get_posts(
-		[
-			'post_type' => 'sws_sitewide_sale',
-			'post_status' => 'publish',
-			'numberposts' => -1,
-		]
-	);
-	$active_sitewide_sale = $options['active_sitewide_sale_id'];
-	echo '<table><tr><td><h3>' . esc_html__( 'Choose Sitewide Sale to View Reports For', 'pmpro-sitewide-sale' ) . ': </h3></td><td><select id="pmpro_sws_sitewide_sale_select">';
-
-	foreach ( $sitewide_sales as $sitewide_sale ) {
-		$selected_modifier = '';
-		if ( $sitewide_sale->ID . '' === $active_sitewide_sale . '' ) {
-			$selected_modifier = ' selected="selected"';
-		}
-		echo '<option value = ' . esc_html( $sitewide_sale->ID ) . esc_html( $selected_modifier ) . '>' . esc_html( get_the_title( $sitewide_sale->ID ) ) . '</option>';
-	}
-	echo '</select></td></tr></table>';
-	echo '<div id="pmpro_sws_reports_container">';
-	echo PMPro_SWS_Reports::get_report_for_code();
-	echo '</div>';
 }
