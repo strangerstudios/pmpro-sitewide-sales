7d07a07560d1bc37d70d8c24d364dd4a036bbf3b
Kimberly Coleman <kim@strangerstudios.com>
2018-10-22 15:39:32 -0400

    Fixing query to use new prefixed meta key for display_post_states and page_row_actions filters

--- includes/classes/class-pmpro-sws-landing-pages.php
+++ includes/classes/class-pmpro-sws-landing-pages.php
@@ -180,7 +180,7 @@ class PMPro_SWS_Landing_Pages {
 		$sitewide_sale = get_posts(
 			array(
 				'post_type'      => 'sws_sitewide_sale',
-				'meta_key'       => 'landing_page_post_id',
+				'meta_key'       => 'pmpro_sws_landing_page_post_id',
 				'meta_value'     => '' . $post->ID,
 				'posts_per_page' => 1,
 			)
@@ -203,7 +203,7 @@ class PMPro_SWS_Landing_Pages {
 		$sitewide_sale = get_posts(
 			array(
 				'post_type'      => 'sws_sitewide_sale',
-				'meta_key'       => 'landing_page_post_id',
+				'meta_key'       => 'pmpro_sws_landing_page_post_id',
 				'meta_value'     => '' . $post->ID,
 				'posts_per_page' => 1,
 			)
