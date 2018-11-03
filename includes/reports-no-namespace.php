325eaf011a12778f205536ff5947913af730c776
Jason Coleman <jason@strangerstudios.com>
2018-11-03 04:41:41 -0400

    Continued improving reports screen appearance

--- includes/reports-no-namespace.php
+++ includes/reports-no-namespace.php
@@ -74,8 +74,11 @@ function pmpro_report_pmpro_sws_reports_page() {
 				<input type="submit" class="button" value="<?php echo esc_attr_e( 'Generate Report', 'pmpro-sitewide-sales' );?>" />
 			</li>
 		</ul>
-		<br /><br />
-		<hr class="clear" />
+		<div class="clear"></div>
+		<hr />
+		<br />
+		<h1 class="wp-heading-inline"><?php echo get_the_title( $sitewide_sale_id ); ?></h1>
+		<a href="<?php edit_post_link( $sitewide_sale_id ); ?>" class="page-title-action"><?php esc_html_e( 'Edit', 'pmpro-sitewide-sale' ); ?></a>
 		<p>
 		<?php
 			printf( wp_kses_post( 'From %s to %s using discount code %s on landing page <a target="_blank" href="%s">%s</a>.', 'pmpro-sitewide-sales' ),
@@ -87,51 +90,71 @@ function pmpro_report_pmpro_sws_reports_page() {
 				);
 		?>
 		</p>
-		<hr />
 		<style>
 		.pmpro_sws_reports-box {
 			background: #FFF;
 			border: 1px solid #CCC;
-			margin-bottom: 2rem;
-			padding: 20px;
+			margin: 2rem 0;
+			padding: 2rem;
 			text-align: center;
 		}
 		.pmpro_sws_reports-box h1.pmpro_sws_reports-box-title {
-			border-bottom: 1px solid #EFEFEF;
 			color: #999;
 			font-family: Georgia, Times, "Times New Roman", serif;
-			margin: 0 0 1rem 0;
-			padding: 0 0 1rem 0;
+			margin: 0;
+			padding: 0;
+		}
+		.pmpro_sws_reports-box hr {
+			margin: 2rem 0;
 		}
 		.pmpro_sws_reports-data {
 			display: grid;
 			grid-gap: 1rem;
+		}
+		.pmpro_sws_reports-data-3col {
 			grid-template-columns: 1fr 1fr 1fr;
+		}
+		.pmpro_sws_reports-data-4col {
+			grid-template-columns: 1fr 1fr 1fr 1fr;
 		}
 		.pmpro_sws_reports-data-section {
 		}
 		.pmpro_sws_reports-data-section h1 {
-			font-size: 40px;
-			line-height: 50px;
+			font-size: 30px;
+			line-height: 40px;
 			margin: 0;
 		}
-		.pmpro_sws_reports-data-section-sub {
-			display: grid;
-			grid-gap: 1rem;
-			grid-template-columns: 3fr 1fr;
-			margin: 1rem 0;
-			text-align: left;
-		}
-		.pmpro_sws_reports-data-section-sub .pmpro_sws_reports-data-label {
-			text-align: right;
+		@media screen and (max-width: 768px) {
+			.pmpro_sws_reports-box {
+				margin: 0 1rem;
+				padding: 1rem;
+			}
+			.pmpro_sws_reports-box hr {
+				margin: .5rem 0;
+			}
+			.pmpro_sws_reports-data {
+				display: block;
+			}
+			.pmpro_sws_reports-data-section h1,
+			.pmpro_sws_reports-data-section p {
+				display: inline-block;
+				font-size: 18px;
+			}
+			.pmpro_sws_reports-data-section br {
+				display: none;
+			}
+			.pmpro_sws_reports-data-section h1:after {
+				content: ": ";
+			}
 		}
 		</style>
 		<div class="pmpro_sws_reports-box">
 			<h1 class="pmpro_sws_reports-box-title"><?php esc_html_e( 'Overall Sale Performance', 'pmpro-sitewide-sales' ); ?></h1>
-			<div class="pmpro_sws_reports-data">
+			<hr />
+			<div class="pmpro_sws_reports-data pmpro_sws_reports-data-3col">
 				<div class="pmpro_sws_reports-data-section">
 					<h1><?php echo esc_attr( $stats['new_rev_with_code'] ); ?></h1>
-					<p><?php esc_html_e( 'Total Sales Revenue Using Discount', 'pmpro-sitewide-sales' ); ?></p>
+					<p><?php esc_html_e( 'Sales With Discount', 'pmpro-sitewide-sales' ); ?></p>
 				</div>
 				<div id="pmpro_sws_reports-data-section_banner" class="pmpro_sws_reports-data-section">
 					<h1><?php echo esc_attr( $stats['banner_impressions'] ); ?></h1>
@@ -145,7 +168,8 @@ function pmpro_report_pmpro_sws_reports_page() {
 		</div>
 		<div class="pmpro_sws_reports-box">
 			<h1 class="pmpro_sws_reports-box-title"><?php esc_html_e( 'Sales Comparision Data', 'pmpro-sitewide-sales' ); ?></h1>
-			<div class="pmpro_sws_reports-data">
+			<hr />
+			<div class="pmpro_sws_reports-data pmpro_sws_reports-data-3col">
 				<div class="pmpro_sws_reports-data-section">
 					<h1><?php echo esc_attr( $stats['checkout_conversions_with_code'] ); ?></h1>
 					<p><?php esc_html_e( 'Checkouts Using Discount', 'pmpro-sitewide-sales' ); ?></p>
@@ -160,20 +184,39 @@ function pmpro_report_pmpro_sws_reports_page() {
 				</div>
 			</div>
 			<hr />
-			<div class="pmpro_sws_reports-data">
+			<div class="pmpro_sws_reports-data pmpro_sws_reports-data-4col">
+				<?php
+					$total_sales_in_period = $stats['new_rev_with_code'] + $stats['new_rev_without_code'] + $stats['old_rev'];
+				?>
 				<div class="pmpro_sws_reports-data-section">
-					<span class="pmpro_sws_reports-data-label"><?php esc_html_e( 'Sales Without Discount in Period', 'pmpro-sitewide-sales' ); ?></span>
-					<span class="pmpro_sws_reports-data-value"><?php echo esc_attr( $stats['new_rev_without_code'] ); ?></span>
+					<h1><?php echo esc_attr( $stats['new_rev_with_code'] ); ?></h1>
+					<p>
+						<?php esc_html_e( 'Sales With Discount', 'pmpro-sitewide-sales' ); ?>
+						<br />
+						(<?php echo round ( PMPro_Sitewide_Sales\includes\classes\PMPro_SWS_Reports::divide_into_percent( $stats['new_rev_with_code'], $total_sales_in_period ), 2 ); ?>%)
+					</p>
 				</div>
 				<div class="pmpro_sws_reports-data-section">
-					<span class="pmpro_sws_reports-data-label"><?php esc_html_e( 'Other Sales (Renewals) in Period', 'pmpro-sitewide-sales' ); ?></span>
-					<span class="pmpro_sws_reports-data-value"><?php echo esc_attr( $stats['old_rev'] ); ?></span>
+					<h1><?php echo esc_attr( $stats['new_rev_without_code'] ); ?></h1>
+					<p>
+						<?php esc_html_e( 'Sales Without Discount', 'pmpro-sitewide-sales' ); ?>
+						<br />
+						(<?php echo round ( PMPro_Sitewide_Sales\includes\classes\PMPro_SWS_Reports::divide_into_percent( $stats['new_rev_without_code'], $total_sales_in_period ), 2 ); ?>%)
+					</p>
 				</div>
-				<div class="pmpro_sws_reports-data-section">	
-					<span class="pmpro_sws_reports-data-label"><?php esc_html_e( 'Total Sales in Period', 'pmpro-sitewide-sales' ); ?></span>
-					<span class="pmpro_sws_reports-data-value"><?php echo esc_attr( $stats['new_rev_with_code'] ) + esc_attr( $stats['new_rev_without_code'] ) + esc_attr( $stats['old_rev'] ); ?></span>
+				<div class="pmpro_sws_reports-data-section">
+					<h1><?php echo esc_attr( $stats['old_rev'] ); ?></h1>
+					<p>
+						<?php esc_html_e( 'Other Sales (including Renewals)', 'pmpro-sitewide-sales' ); ?>
+						<br />
+						(<?php echo round ( PMPro_Sitewide_Sales\includes\classes\PMPro_SWS_Reports::divide_into_percent( $stats['old_rev'], $total_sales_in_period ), 2 ); ?>%)
+					</p>
 				</div>
-			</div>			
+				<div class="pmpro_sws_reports-data-section">
+					<h1><?php echo esc_attr( $stats['new_rev_with_code'] ) + esc_attr( $stats['new_rev_without_code'] ) + esc_attr( $stats['old_rev'] ); ?></h1>
+					<p><?php esc_html_e( 'Total Sales in Period', 'pmpro-sitewide-sales' ); ?></p>
+				</div>
+			</div>
 		</div>
 		<pre>
 			<?php var_dump( $stats ); ?>
