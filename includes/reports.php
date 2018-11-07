512fc5510d39eea6d400a5d8b53818f9535ed98e
Paul Barthmaier <paul@barthmaier.rocks>
2018-11-07 11:57:41 -0500

    wpcs pass

--- includes/reports.php
+++ includes/reports.php
@@ -1,21 +1,22 @@
 <?php
-
 /**
  * Report Widget, needs to be outside namespace because
  * function name is 'calculated' in core
+ *
+ * @package pmpro-sitewide-sales
  */
 function pmpro_report_pmpro_sws_reports_widget() {
 	esc_html_e( 'View reports for your most recent sales.', 'pmpro-sitewide-sale' );
 	$sitewide_sales = get_posts(
 		[
-			'post_type' => 'pmpro_sitewide_sale',
+			'post_type'   => 'pmpro_sitewide_sale',
 			'post_status' => 'publish',
 			'numberposts' => 5,
-			'orderby' => 'ID',
-			'order' => 'DESC',
+			'orderby'     => 'ID',
+			'order'       => 'DESC',
 		]
 	);
-	if ( ! empty ( $sitewide_sales ) ) {
+	if ( ! empty( $sitewide_sales ) ) {
 		foreach ( $sitewide_sales as $sitewide_sale ) {
 			echo '<p>';
 			echo '<strong><a href="' . admin_url( 'admin.php?page=pmpro-reports&report=pmpro_sws_reports&pmpro_sws_sitewide_sale_id=' . $sitewide_sale->ID ) . '">' . esc_html( get_the_title( $sitewide_sale->ID ) ) . '</a></strong>';
@@ -35,17 +36,17 @@ function pmpro_report_pmpro_sws_reports_widget() {
  */
 function pmpro_report_pmpro_sws_reports_page() {
 	global $wpdb;
-	$options = PMPro_Sitewide_Sales\includes\classes\PMPro_SWS_Settings::get_options();
+	$options        = PMPro_Sitewide_Sales\includes\classes\PMPro_SWS_Settings::get_options();
 	$sitewide_sales = get_posts(
 		[
-			'post_type' => 'pmpro_sitewide_sale',
+			'post_type'   => 'pmpro_sitewide_sale',
 			'post_status' => 'publish',
 			'numberposts' => -1,
-			'orderby' => 'ID',
-			'order' => 'DESC',
+			'orderby'     => 'ID',
+			'order'       => 'DESC',
 		]
 	);
-	if( ! empty( $_REQUEST['pmpro_sws_sitewide_sale_id'] ) ) {
+	if ( ! empty( $_REQUEST['pmpro_sws_sitewide_sale_id'] ) ) {
 		$sitewide_sale_id = intval( $_REQUEST['pmpro_sws_sitewide_sale_id'] );
 	} else {
 		$sitewide_sale_id = $options['active_sitewide_sale_id'];
@@ -56,22 +57,22 @@ function pmpro_report_pmpro_sws_reports_page() {
 	?>
 	<form id="posts-filter" method="get" action="">
 		<h1>
-			<?php esc_html_e( 'Sitewide Sales Report', 'pmpro-sitewide-sales' );?>
+			<?php esc_html_e( 'Sitewide Sales Report', 'pmpro-sitewide-sales' ); ?>
 		</h1>
 		<ul class="subsubsub">
 			<li>
-				<?php esc_html_e( 'Show reports for ', 'pmpro-sitewide-sales' );?>
+				<?php esc_html_e( 'Show reports for ', 'pmpro-sitewide-sales' ); ?>
 				<select name="pmpro_sws_sitewide_sale_id">
 				<?php
-					foreach ( $sitewide_sales as $sitewide_sale ) {
-						echo '<option value="' . esc_attr( $sitewide_sale->ID ) . '" ' . selected( $sitewide_sale_id, $sitewide_sale->ID ) . '>' . esc_html( get_the_title( $sitewide_sale->ID ) ) . '</option>';
-					}
+				foreach ( $sitewide_sales as $sitewide_sale ) {
+					echo '<option value="' . esc_attr( $sitewide_sale->ID ) . '" ' . selected( $sitewide_sale_id, $sitewide_sale->ID ) . '>' . esc_html( get_the_title( $sitewide_sale->ID ) ) . '</option>';
+				}
 				?>
 				</select>
 
 				<input type="hidden" name="page" value="pmpro-reports" />
 				<input type="hidden" name="report" value="pmpro_sws_reports" />
-				<input type="submit" class="button" value="<?php echo esc_attr_e( 'Generate Report', 'pmpro-sitewide-sales' );?>" />
+				<input type="submit" class="button" value="<?php echo esc_attr_e( 'Generate Report', 'pmpro-sitewide-sales' ); ?>" />
 			</li>
 		</ul>
 		<div class="clear"></div>
@@ -83,10 +84,11 @@ function pmpro_report_pmpro_sws_reports_page() {
 			<h1 class="pmpro_sws_reports-box-title"><?php esc_html_e( 'Overall Sale Performance', 'pmpro-sitewide-sales' ); ?></h1>
 			<p>
 			<?php
-				printf( wp_kses_post( 'All visitors from %s to %s.', 'pmpro-sitewide-sales' ),
-						date( get_option( 'date_format' ), strtotime( $stats['start_date'], current_time( 'timestamp' ) ) ),
-						date( get_option( 'date_format' ), strtotime( $stats['end_date'], current_time( 'timestamp' ) ) )
-					);
+				printf(
+					wp_kses_post( 'All visitors from %s to %s.', 'pmpro-sitewide-sales' ),
+					date( get_option( 'date_format' ), strtotime( $stats['start_date'], current_time( 'timestamp' ) ) ),
+					date( get_option( 'date_format' ), strtotime( $stats['end_date'], current_time( 'timestamp' ) ) )
+				);
 			?>
 			</p>
 			<hr />
@@ -99,17 +101,20 @@ function pmpro_report_pmpro_sws_reports_page() {
 					<h1><?php echo esc_attr( $stats['landing_page_visits'] ); ?></h1>
 					<p>
 						<?php
-							printf( wp_kses_post( '<a href="%s" title="%s">Landing</a> Page Visits', 'pmpro-sitewide-sales' ),
+							printf(
+								wp_kses_post( '<a href="%s" title="%s">Landing</a> Page Visits', 'pmpro-sitewide-sales' ),
 								$stats['landing_page_url'],
 								$stats['landing_page_title']
-							); ?>
+							);
+						?>
 					</p>
 				</div>
 				<div id="pmpro_sws_reports-data-section_sales" class="pmpro_sws_reports-data-section">
 					<h1><?php echo esc_attr( $stats['checkout_conversions_with_code'] ); ?></h1>
 					<p>
 						<?php
-							printf( wp_kses_post( 'Checkouts using <a href="%s">%s</a>', 'pmpro-sitewide-sales' ),
+							printf(
+								wp_kses_post( 'Checkouts using <a href="%s">%s</a>', 'pmpro-sitewide-sales' ),
 								admin_url( 'admin.php?page=pmpro-discountcodes&edit=' . $stats['discount_code_id'] ),
 								$stats['discount_code']
 							);
@@ -126,10 +131,11 @@ function pmpro_report_pmpro_sws_reports_page() {
 			<h1 class="pmpro_sws_reports-box-title"><?php esc_html_e( 'Revenue Breakdown', 'pmpro-sitewide-sales' ); ?></h1>
 			<p>
 				<?php
-				printf( wp_kses_post( 'All orders from %s to %s.', 'pmpro-sitewide-sales' ),
-						date( get_option( 'date_format' ), strtotime( $stats['start_date'], current_time( 'timestamp' ) ) ),
-						date( get_option( 'date_format' ), strtotime( $stats['end_date'], current_time( 'timestamp' ) ) )
-					);
+				printf(
+					wp_kses_post( 'All orders from %s to %s.', 'pmpro-sitewide-sales' ),
+					date( get_option( 'date_format' ), strtotime( $stats['start_date'], current_time( 'timestamp' ) ) ),
+					date( get_option( 'date_format' ), strtotime( $stats['end_date'], current_time( 'timestamp' ) ) )
+				);
 				?>
 			</p>
 			<hr />
@@ -139,7 +145,7 @@ function pmpro_report_pmpro_sws_reports_page() {
 					<p>
 						<?php esc_html_e( 'Sale Revenue', 'pmpro-sitewide-sales' ); ?>
 						<br />
-						(<?php echo round ( PMPro_Sitewide_Sales\includes\classes\PMPro_SWS_Reports::divide_into_percent( $stats['new_rev_with_code'], $stats['total_rev'] ), 2 ); ?>%)
+						(<?php echo round( PMPro_Sitewide_Sales\includes\classes\PMPro_SWS_Reports::divide_into_percent( $stats['new_rev_with_code'], $stats['total_rev'] ), 2 ); ?>%)
 					</p>
 				</div>
 				<div class="pmpro_sws_reports-data-section">
@@ -147,7 +153,7 @@ function pmpro_report_pmpro_sws_reports_page() {
 					<p>
 						<?php esc_html_e( 'Other New Revenue', 'pmpro-sitewide-sales' ); ?>
 						<br />
-						(<?php echo round ( PMPro_Sitewide_Sales\includes\classes\PMPro_SWS_Reports::divide_into_percent( $stats['new_rev_without_code'], $stats['total_rev'] ), 2 ); ?>%)
+						(<?php echo round( PMPro_Sitewide_Sales\includes\classes\PMPro_SWS_Reports::divide_into_percent( $stats['new_rev_without_code'], $stats['total_rev'] ), 2 ); ?>%)
 					</p>
 				</div>
 				<div class="pmpro_sws_reports-data-section">
@@ -155,7 +161,7 @@ function pmpro_report_pmpro_sws_reports_page() {
 					<p>
 						<?php esc_html_e( 'Renewals', 'pmpro-sitewide-sales' ); ?>
 						<br />
-						(<?php echo round ( PMPro_Sitewide_Sales\includes\classes\PMPro_SWS_Reports::divide_into_percent( $stats['old_rev'], $stats['total_rev'] ), 2 ); ?>%)
+						(<?php echo round( PMPro_Sitewide_Sales\includes\classes\PMPro_SWS_Reports::divide_into_percent( $stats['old_rev'], $stats['total_rev'] ), 2 ); ?>%)
 					</p>
 				</div>
 				<div class="pmpro_sws_reports-data-section">
