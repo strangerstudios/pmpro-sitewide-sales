eee9f0dd932b2c3082964763a5756c4cccb1e900
Kimberly Coleman <kim@strangerstudios.com>
2018-11-17 10:03:05 -0500

    Adding button to view detailed report for compatibility with PMPro 2.0; Adding logic for case of "no sales found"

--- includes/reports.php
+++ includes/reports.php
@@ -4,29 +4,42 @@
  * Report Widget, needs to be outside namespace because
  * function name is 'calculated' in core
  */
-function pmpro_report_pmpro_sws_reports_widget() {
-	esc_html_e( 'View reports for your most recent sales.', 'pmpro-sitewide-sale' );
-	$sitewide_sales = get_posts(
-		[
-			'post_type'   => 'pmpro_sitewide_sale',
-			'post_status' => 'publish',
-			'numberposts' => 5,
-			'orderby'     => 'ID',
-			'order'       => 'DESC',
-		]
-	);
-	if ( ! empty( $sitewide_sales ) ) {
-		foreach ( $sitewide_sales as $sitewide_sale ) {
-			echo '<p>';
-			echo '<strong><a href="' . admin_url( 'admin.php?page=pmpro-reports&report=pmpro_sws_reports&pmpro_sws_sitewide_sale_id=' . $sitewide_sale->ID ) . '">' . esc_html( get_the_title( $sitewide_sale->ID ) ) . '</a></strong>';
-			echo ' (';
-			echo date_i18n( get_option( 'date_format' ), strtotime( $sitewide_sale->pmpro_sws_start_date, current_time( 'timestamp' ) ) );
-			echo ' - ';
-			echo date_i18n( get_option( 'date_format' ), strtotime( $sitewide_sale->pmpro_sws_end_date, current_time( 'timestamp' ) ) );
-			echo ')';
-			echo '</p>';
-		}
-	}
+function pmpro_report_pmpro_sws_reports_widget() { ?>
+	<span id="pmpro_sws_reports" class="pmpro_report-holder">
+	<?php
+		$sitewide_sales = get_posts(
+			[
+				'post_type'   => 'pmpro_sitewide_sale',
+				'post_status' => 'publish',
+				'numberposts' => 5,
+				'orderby'     => 'ID',
+				'order'       => 'DESC',
+			]
+		);
+		if ( ! empty( $sitewide_sales ) ) {
+			esc_html_e( 'View reports for your most recent sales.', 'pmpro-sitewide-sale' );
+			foreach ( $sitewide_sales as $sitewide_sale ) {
+				echo '<p>';
+				echo '<strong><a href="' . admin_url( 'admin.php?page=pmpro-reports&report=pmpro_sws_reports&pmpro_sws_sitewide_sale_id=' . $sitewide_sale->ID ) . '">' . esc_html( get_the_title( $sitewide_sale->ID ) ) . '</a></strong>';
+				echo ' (';
+				echo date_i18n( get_option( 'date_format' ), strtotime( $sitewide_sale->pmpro_sws_start_date, current_time( 'timestamp' ) ) );
+				echo ' - ';
+				echo date_i18n( get_option( 'date_format' ), strtotime( $sitewide_sale->pmpro_sws_end_date, current_time( 'timestamp' ) ) );
+				echo ')';
+				echo '</p>';
+			} ?>
+			<hr />
+			<p class="pmpro_report-button">
+				<a class="button button-primary" href="<?php echo admin_url( 'admin.php?page=pmpro-reports&report=pmpro_sws_reports' ); ?>"><?php _e('Details', 'paid-memberships-pro' );?></a>
+			</p>
+			<?php
+		} else { ?>
+			<?php esc_html_e( 'No sales found.', 'pmpro-sitewide-sale' ); ?>
+			<a href="<?php echo esc_url( admin_url( 'post-new.php?post_type=pmpro_sitewide_sale' ) ); ?>"><?php esc_html_e( 'Create a Sitewide Sale', 'pmpro-sitewide-sales' ); ?></a>
+			<?php
+		} ?>
+	</span>
+	<?php
 }
 
 /**
